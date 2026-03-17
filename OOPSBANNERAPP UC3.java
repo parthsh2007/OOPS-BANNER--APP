f12ef240e55b899384e5f7ab7cc3c40df5c246d3
parthsh2007 <parthsharma200725@gmail.com>
2026-03-17 23:07:05 +0530

    Add files via upload

---
+++ OOPSBANNERAPP UC3.java
@@ -0,0 +1,30 @@
+/**
+ * OOPSBannerApp
+ * UC3: Optimized banner using String.join()
+ * 
+ * @author Parth
+ * @version 3.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        // Each line built using String.join()
+        String line1 = String.join("", " ***** ", "   ", " ***** ", "   ", " ****** ", "   ", " ***** ");
+        String line2 = String.join("", "*     *", "  ", "*     *", "   ", "*     *", "  ", "*     *");
+        String line3 = String.join("", "*     *", "  ", "*     *", "   ", "*     *", "  ", "*      ");
+        String line4 = String.join("", "*     *", "  ", "*     *", "   ", "****** ", "   ", " ***** ");
+        String line5 = String.join("", "*     *", "  ", "*     *", "   ", "*      ", "      ", "*");
+        String line6 = String.join("", "*     *", "  ", "*     *", "   ", "*      ", "   ", "*     *");
+        String line7 = String.join("", " ***** ", "   ", " ***** ", "   ", "*      ", "    ", " ***** ");
+
+        // Print banner
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+        System.out.println(line6);
+        System.out.println(line7);
+    }
+}
