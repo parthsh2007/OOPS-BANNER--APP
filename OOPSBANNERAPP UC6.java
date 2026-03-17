4bff09e598c9fab59d29cc20de3dc97ffea0f1de
parthsh2007 <parthsharma200725@gmail.com>
2026-03-17 23:53:07 +0530

    Add files via upload

---
+++ OOPSBANNERAPP UC6.java
@@ -0,0 +1,60 @@
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        String[] o = getO();
+        String[] p = getP();
+        String[] s = getS();
+
+        // Combine letters line by line
+        String[] banner = new String[7];
+
+        for (int i = 0; i < 7; i++) {
+            banner[i] = String.join("   ", o[i], o[i], p[i], s[i]);
+        }
+
+        // Print banner
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+
+    // Helper method for 'O'
+    public static String[] getO() {
+        return new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        };
+    }
+
+    // Helper method for 'P'
+    public static String[] getP() {
+        return new String[]{
+            "****** ",
+            "*     *",
+            "*     *",
+            "****** ",
+            "*      ",
+            "*      ",
+            "*      "
+        };
+    }
+
+    // Helper method for 'S'
+    public static String[] getS() {
+        return new String[]{
+            " ***** ",
+            "*     *",
+            "*      ",
+            " ***** ",
+            "      *",
+            "*     *",
+            " ***** "
+        };
+    }
+}
